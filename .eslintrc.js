76f256328d574a802bf1a9bba0c1382fa027ea06
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-01-28 13:06:30 +0300

    work environment improvement

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'block-spacing': ['error', 'always'],
+    'object-curly-spacing': ['error', 'always'],
+  },
 };
