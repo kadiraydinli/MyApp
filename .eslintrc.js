38daf43de40c41bc05646459c6f199295c952e6d
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-01-28 16:49:11 +0300

    eslint-plugin-unused-imports installed

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,24 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    'block-spacing': ['error', 'always'],
-    'object-curly-spacing': ['error', 'always'],
+    indent: ['error', 2, { VariableDeclarator: 2 }],
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'sort-imports': [
+      'error',
+      {
+        ignoreDeclarationSort: true,
+      },
+    ],
   },
+  plugins: ['unused-imports'],
 };
