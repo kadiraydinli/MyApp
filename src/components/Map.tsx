d9afeb8cdea56eebfbcbc08dfee64a848a6b565c
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-01-28 17:32:45 +0300

    add map theme

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { MapStyleElement, PROVIDER_GOOGLE } from 'react-native-maps';
+import mapTheme from '../config/mapTheme.json';
 
 type MapProps = {};
 
 const Map: React.FC<MapProps> = () => {
+  const customMapStyle: MapStyleElement[] = mapTheme;
   return (
     <MapView
       initialRegion={{
@@ -13,7 +15,9 @@ const Map: React.FC<MapProps> = () => {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       }}
+      provider={PROVIDER_GOOGLE}
       style={styles.container}
+      customMapStyle={customMapStyle}
     />
   );
 };
